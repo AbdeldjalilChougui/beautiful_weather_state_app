3c82894e89886384fc781a96be93227b4a6ea512
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-05-03 07:46:53 +0100

    commit weather repo

---
+++ lib/WeatherRepo.dart
@@ -0,0 +1,24 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'WeatherModel.dart';
+
+class WeatherRepo {
+
+  Future<WeatherModel> getWeather(String city) async {
+    final result = await http.Client().get("https://api.openweathermap.org/data/2.5/weather?q=$city&APPID=64a9a079e7a2770ebb2a83eff61a0600");
+
+    if (result.statusCode != 200) {
+      throw Exception();
+    }
+
+    return parsedJson(result.body);
+  }
+
+  WeatherModel parsedJson(final response) {
+    final jsonDecoded = json.decode(response);
+
+    final jsonWeather = jsonDecoded["main"];
+
+    return WeatherModel.fromJson(jsonWeather);
+  }
+}
