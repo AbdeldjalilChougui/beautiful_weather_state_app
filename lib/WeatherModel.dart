0be4d7c79f9ff57e75d01b73c4f6da1aace8fb53
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-05-03 07:46:47 +0100

    commit weather model

---
+++ lib/WeatherModel.dart
@@ -0,0 +1,25 @@
+class WeatherModel {
+  final temp;
+  final pressure;
+  final humidity;
+  final tempMax;
+  final tempMin;
+
+  double get getTemp => temp - 272.5;
+
+  double get getMaxTemp => tempMax - 272.5;
+
+  double get getMinTemp => tempMin - 272.5;
+
+  WeatherModel(this.temp, this.pressure, this.humidity, this.tempMax, this.tempMin);
+
+  factory WeatherModel.fromJson(Map<String, dynamic> json){
+    return WeatherModel(
+      json["temp"],
+      json["pressure"],
+      json["humidity"],
+      json["temp_max"],
+      json["temp_min"],
+    );
+  }
+}
