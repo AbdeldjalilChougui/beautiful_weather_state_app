ca90dc7f0d095ffd4be67ae72083bf252c0aab09
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-09-01 21:31:48 +0100

    commit refactor

--- lib/WeatherRepo.dart
+++ lib/WeatherRepo.dart
@@ -5,7 +5,7 @@ import 'WeatherModel.dart';
 class WeatherRepo {
 
   Future<WeatherModel> getWeather(String city) async {
-    final result = await http.Client().get("https://api.openweathermap.org/data/2.5/weather?q=$city&APPID=64a9a079e7a2770ebb2a83eff61a0600");
+    final result = await http.Client().get(Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$city&APPID=64a9a079e7a2770ebb2a83eff61a0600"));
 
     if (result.statusCode != 200) {
       throw Exception();
